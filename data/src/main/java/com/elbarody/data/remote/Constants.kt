554712b826683cd31bc0a05e3fd58277ae63401c
Elbarody <ahmedelbarody999@gmail.com>
2024-07-31 00:49:11 +0300

    integration with twitter post tweet

---
+++ data/src/main/java/com/elbarody/data/remote/Constants.kt
@@ -0,0 +1,14 @@
+package com.elbarody.data.remote
+
+object Constants {
+
+    const val BEARER_TOKEN="" //enter your bearer
+
+
+    const val BASE_URL = "https://api.twitter.com/"
+
+    const val OAUTH_CONSUMER_KEY = "" //api key
+    const val OAUTH_CONSUMER_SECRET = ""//api secret key
+    const val OAUTH_TOKEN = ""//api token
+    const val OAUTH_TOKEN_SECRET = ""// api secret token
+}
