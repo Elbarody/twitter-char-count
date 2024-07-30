28ab82c98b6229540bbb3491739aee23f6cc7d89
Elbarody <ahmedelbarody999@gmail.com>
2024-07-30 17:26:49 +0300

    twitter finishing design

---
+++ base/src/main/java/com/elbarody/base/compose/DefaultButton.kt
@@ -0,0 +1,57 @@
+package com.elbarody.base.compose
+
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.elbarody.base.theme.Gray200
+import com.elbarody.base.theme.Typography
+import com.elbarody.base.utils.Dimens
+
+@Composable
+fun DefaultButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true,
+    color: Color = Blue,
+    contentColor: Color = White,
+    minHeight: Dp = 40.dp,
+) {
+
+    val bgColor = if (enabled) color else Gray200
+    val contentColorStatus = if (enabled) contentColor else Black
+    Button(
+        onClick = { if (enabled) onClick() },
+        modifier = modifier.heightIn(min = minHeight),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = bgColor, contentColor = contentColorStatus
+        ),
+        shape = RoundedCornerShape(Dimens.threeLevelPadding)
+    ) {
+        Text(
+            text = text,
+            style = Typography.titleLarge.copy(fontSize = 18.sp, color = contentColorStatus),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DefaultButtonMinHeightPreview() {
+    DefaultButton(text = "post", onClick = {})
+}
--- post-counter/src/main/java/com/elbarody/post_counter/ui/TwitterCounterScreen.kt
+++ post-counter/src/main/java/com/elbarody/post_counter/ui/TwitterCounterScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
@@ -12,14 +13,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,6 +30,7 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -38,8 +41,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elbarody.base.compose.AppScaffold
+import com.elbarody.base.compose.DefaultButton
 import com.elbarody.base.compose.TwitterCounterTopBar
+import com.elbarody.base.theme.Blue500
 import com.elbarody.base.theme.DarkGray
+import com.elbarody.base.theme.DarkGreen
 import com.elbarody.base.theme.ErrorHint
 import com.elbarody.base.theme.Gray200
 import com.elbarody.base.theme.Gray800
@@ -81,17 +87,64 @@ fun TwitterCounterContent(modifier: Modifier) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(Dimens.fiveLevelPadding)
         ) {
-            CounterItem()
+            val charTypedCount by remember {
+                mutableIntStateOf(0)
+            }
 
-            CounterItem()
+            val remainingCount by remember {
+                mutableIntStateOf(280)
+            }
+
+            val charTypedText by remember {
+                mutableStateOf("$charTypedCount/280")
+            }
+            CounterItem(stringResource(R.string.characters_typed),charTypedText)
+
+            CounterItem(stringResource(id = R.string.characters_remaining),remainingCount.toString())
         }
 
-        TwitterPostContent()
+        TwitterPostTextField()
+
+        Spacer(modifier = Modifier.padding(top = Dimens.sixLevelPadding))
+
+        ActionButtons()
     }
 }
 
 @Composable
-fun TwitterPostContent() {
+fun ActionButtons() {
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        DefaultButton(
+            text = stringResource(R.string.copy_text),
+            onClick = { /*TODO*/ },
+            color = DarkGreen,
+            minHeight = 40.dp,
+            modifier = Modifier.wrapContentSize()
+        )
+
+        DefaultButton(
+            text = stringResource(R.string.clear_text),
+            onClick = { /*TODO*/ },
+            color = Color.Red,
+            modifier = Modifier.wrapContentSize(),
+            minHeight = 40.dp
+        )
+    }
+
+    Spacer(modifier = Modifier.padding(top = Dimens.sixLevelPadding))
+
+    DefaultButton(
+        text = stringResource(R.string.post_tweet),
+        onClick = { /*TODO*/ },
+        color = Blue500,
+        modifier = Modifier.fillMaxWidth(),
+        minHeight = 56.dp
+    )
+}
+
+@Composable
+fun TwitterPostTextField() {
     val postText = remember { mutableStateOf(TextFieldValue("")) }
 
     val focusRequester = remember {
@@ -147,13 +200,13 @@ fun TwitterPostContent() {
 @Composable
 fun TwitterPlaceHolder() {
     Text(
-        text = "Start typing! You can enter up to 280 characters",
+        text = stringResource(R.string.typing_hint),
         style = Typography.bodySmall.copy(color = Gray800)
     )
 }
 
 @Composable
-fun RowScope.CounterItem() {
+fun RowScope.CounterItem(title :String,typingValue :String) {
     Column(
         modifier = Modifier
             .weight(1f)
@@ -174,7 +227,7 @@ fun RowScope.CounterItem() {
                 )
                 .padding(vertical = Dimens.twoLevelPadding)
                 .fillMaxWidth(),
-            text = "Characters Typed",
+            text = title,
             textAlign = TextAlign.Center,
             style = Typography.bodySmall.copy(
                 fontWeight = FontWeight.Medium,
@@ -188,7 +241,7 @@ fun RowScope.CounterItem() {
                     top = Dimens.fourLevelPadding, bottom = Dimens.fiveLevelPadding
                 )
                 .fillMaxWidth(),
-            text = "0/180",
+            text = typingValue,
             textAlign = TextAlign.Center,
             style = Typography.titleMedium.copy(fontSize = 26.sp)
         )
