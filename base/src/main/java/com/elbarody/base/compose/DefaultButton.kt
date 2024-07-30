ad3da24c9e10ea95180da55ba57fcd8df844dfb0
Elbarody <ahmedelbarody999@gmail.com>
2024-07-30 18:53:09 +0300

    remove useless import

--- base/src/main/java/com/elbarody/base/compose/DefaultButton.kt
+++ base/src/main/java/com/elbarody/base/compose/DefaultButton.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.style.TextOverflow
@@ -16,7 +15,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.elbarody.base.theme.Gray200
 import com.elbarody.base.theme.Typography
 import com.elbarody.base.utils.Dimens
 
